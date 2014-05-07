56a2148363b98d06ffcb6b88777055185b1bc927
Lukas Rezek <lukas@miratronix.com>
2014-05-07 00:08:03 -0400

    First Commit

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Node.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Relation.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Auto.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Start.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/End.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Property.php';
+require __DIR__ . '/lib/LRezek/Neo4j/Annotation/Index.php';
