620a9903602c9cb3f5da595ed994418fce4b6396
Lukas Rezek <lukas@miratronix.com>
2014-05-22 20:01:24 -0400

    Manual/documentation updates

--- lib/LRezek/Neo4j/Query/Cypher.php
+++ lib/LRezek/Neo4j/Query/Cypher.php
@@ -1,33 +1,82 @@
 <?php
+/**
+ * Contains the Cypher class.
+ *
+ * @author Lukas Rezek <lukas@miratronix.com>
+ * @license MIT
+ * @filesource
+ * @version GIT: $Id$
+ */
 
 namespace LRezek\Neo4j\Query;
 use Everyman\Neo4j\Node;
 use Everyman\Neo4j\Relationship;
 use Lrezek\Neo4j\EntityManager;
 
+/**
+ * Handles construction and execution of cypher queries.
+ *
+ * This class handles the construction and execution of cypher queries. Most of the methods in this class return the
+ * class itself, allowing for cascading method calls.
+ *
+ * @package Neo4j\Query
+ */
 class Cypher
 {
+    /** @var \Lrezek\Neo4j\EntityManager The entity manager. */
     private $em;
+
+    /** @var array Array fo start clauses. */
     private $start = array();
+
+    /** @var array Array of match clauses. */
     private $match = array();
+
+    /** @var array Array of return clauses. */
     private $return = array();
+
+    /** @var array Array of where clauses. */
     private $where = array();
+
+    /** @var array Array of order clauses. */
     private $order = array();
+
+    /** @var  int Mimics the limit clause. */
     private $limit;
+
+    /** @var \LRezek\Neo4j\Query\ParameterProcessor The parameter processor to use. */
     private $processor;
 
+    /**
+     * Initializes the cypher query with an entity manager and a nbew parameter processor.
+     *
+     * @param EntityManager $em The entity manager to use.
+     */
     function __construct(EntityManager $em)
     {
         $this->em = $em;
         $this->processor = new ParameterProcessor(ParameterProcessor::CYPHER);
     }
 
+    /**
+     * Adds parameters to the queries "start" clause.
+     *
+     * @param string $string The string to add to the start clause.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function start($string)
     {
         $this->start = array_merge($this->start, func_get_args());
         return $this;
     }
 
+    /**
+     * Adds nodes to the queries "start" clause.
+     *
+     * @param string $name The name to use for the nodes.
+     * @param mixed $nodes A single (or multiple) node to start on.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function startWithNode($name, $nodes)
     {
         if (! is_array($nodes)) {
@@ -48,6 +97,14 @@ class Cypher
         return $this;
     }
 
+    /**
+     * Adds a node query to the queries "start" clause.
+     *
+     * @param string $name The name to use for the node.
+     * @param string $index The index of the requested start node.
+     * @param string $query The query parameters to use for the node.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function startWithQuery($name, $index, $query)
     {
         $this->start("$name = node:`$index`('$query')");
@@ -55,6 +112,15 @@ class Cypher
         return $this;
     }
 
+    /**
+     * Adds a node lookup to the queries "start" clause.
+     *
+     * @param string $name The name to use for the node.
+     * @param string $index The index of the requested start node.
+     * @param string $key The key to look for.
+     * @param string $value The value of the key for the lookup.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function startWithLookup($name, $index, $key, $value)
     {
         $this->start("$name = node:`$index`($key = :{$name}_{$key})");
@@ -63,36 +129,73 @@ class Cypher
         return $this;
     }
 
+    /**
+     * Adds parameters to the queries "match" clause.
+     *
+     * @param string $string The string to add to the match clause.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function match($string)
     {
         $this->match = array_merge($this->match, func_get_args());
         return $this;
     }
 
+    /**
+     * Adds parameters to the queries "end" clause.
+     *
+     * @param string $string The string to add to the end clause.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function end($string)
     {
         $this->return = array_merge($this->return, func_get_args());
         return $this;
     }
 
+    /**
+     * Adds parameters to the queries "where" clause.
+     *
+     * @param string $string The string to add to the where clause.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function where($string)
     {
         $this->where = array_merge($this->where, func_get_args());
         return $this;
     }
 
+    /**
+     * Adds parameters to the queries "order" clause.
+     *
+     * @param string $string The string to add to the order clause.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function order($string)
     {
         $this->order = array_merge($this->order, func_get_args());
         return $this;
     }
 
+    /**
+     * Adds a "limit" to the returned results.
+     *
+     * @param int $limit How many entries to limit the results to.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function limit($limit)
     {
         $this->limit = (int) $limit;
         return $this;
     }
 
+    /**
+     * Sets named query parameters.
+     *
+     * @param string $name The name of the parameter.
+     * @param mixed $value The value of the parameter.
+     * @return $this Returns this class to allow for cascading method calls.
+     */
     function set($name, $value)
     {
         $this->processor->setParameter($name, $value);
@@ -100,14 +203,26 @@ class Cypher
         return $this;
     }
 
+    /**
+     * Executes the query and returns one result.
+     *
+     * @return mixed|null The query result.
+     */
     function getOne()
     {
         $result = $this->execute();
-        if (isset($result[0])) {
+
+        if (isset($result[0]))
+        {
             return $this->convertValue($result[0][0]);
         }
     }
 
+    /**
+     * Executes the query and returns a list of results, as entities.
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection The query result.
+     */
     function getList()
     {
         $result = $this->execute();
@@ -122,6 +237,11 @@ class Cypher
         return $list;
     }
 
+    /**
+     * Executes the query and returns a array collection of assoc. arrays, without converting them to entities.
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection The query result.
+     */
     function getResult()
     {
         $result = $this->execute();
@@ -145,6 +265,11 @@ class Cypher
         return $list;
     }
 
+    /**
+     * Executes the query.
+     *
+     * @return \Everyman\Neo4j\Query\ResultSet The query result.
+     */
     private function execute()
     {
         $query = '';
@@ -183,6 +308,12 @@ class Cypher
         return $this->em->cypherQuery($this->processor->getQuery(), $parameters);
     }
 
+    /**
+     * Converts raw query results to node/relation entities.
+     *
+     * @param mixed $value One query result.
+     * @return Node|Relationship The entity.
+     */
     private function convertValue($value)
     {
         if ($value instanceof Node)
--- lib/LRezek/Neo4j/Query/LuceneQueryProcessor.php
+++ lib/LRezek/Neo4j/Query/LuceneQueryProcessor.php
@@ -1,20 +1,36 @@
 <?php
+/**
+ * Contains the LuceneQueryProcessor class.
+ *
+ * @author Christophe Willemsen <willemsen.christophe@gmail.com>
+ * @license MIT
+ * @filesource
+ * @version GIT: $Id$
+ */
 
 namespace LRezek\Neo4j\Query;
 
 /**
- * Class that process requests through the Lucene Engine
- * 
- * @author Christophe Willemsen <willemsen.christophe@gmail.com>
+ * Class that processes requests through the Lucene Engine
+ *
+ * @package Neo4j\Query
  */
 class LuceneQueryProcessor
 {
     const STRICT = 'STRICT_MODE';
 
+    /** @var array List of parameters. */
     protected $_args;
+
+    /** @var string Storage for the built query. */
     protected $query;
+
+    /** @var array List of expanded expressions. */
     protected $allowedExpressions = array('AND' => ' AND ');
 
+    /**
+     * Initializes the query processor.
+     */
     public function __construct()
     {
         foreach($this->allowedExpressions as $expr => $queryExpr) {
@@ -22,16 +38,28 @@ class LuceneQueryProcessor
         }
     }
 
+    /**
+     * Adds a query term to the query, currently only used for "AND" expressions.
+     *
+     * @param string $term The term to add.
+     * @param string $value The value to add for the term.
+     * @param string $expr The expression to use (default = AND).
+     * @param string $mode What mode to use (default = STRICT).
+     */
     public function addQueryTerm($term, $value, $expr = 'AND', $mode = self::STRICT)
     {
-        if($this->isValidExpression($expr)) {
+        if($this->isValidExpression($expr))
+        {
             $method = '_'.strtolower($expr);
             $this->$method($term, $value);
         }
     }
 
     /**
-     * Adds a 'AND' search to the query array
+     * Adds a 'AND' search to the query array.
+     *
+     * @param string $term The term to add.
+     * @param string $value The value fo the term.
      */
     public function _and($term, $value)
     {
@@ -40,14 +68,19 @@ class LuceneQueryProcessor
     }
 
     /**
-     * Embed the value between "" if she contains spaces
-     * If first character is "(" it adds no quotes
+     * Embeds the value between "" if she contains spaces. If first character is "(" it adds no quotes.
+     *
+     * @param string $value The value to prepare.
+     * @return string The prepared value.
      */
      public function prepareValue($value)
     {
         $value = trim($value);
-        if(strpos($value, ' ')) {
+
+        if(strpos($value, ' '))
+        {
             $fl = mb_substr($value, 0, 1, 'UTF-8');
+
             if ($fl != '(')
                 return '"'.$value.'"';
             else
@@ -57,18 +90,25 @@ class LuceneQueryProcessor
     }
 
     /**
-     * Checks whether or not the given expression is valid
+     * Checks whether or not the given expression is valid.
+     *
+     * @param string $expr The expression to check.
+     * @return bool Whether the expression is valid.
+     * @throws \InvalidArgumentException thrown if the expression is invalid.
      */
     public function isValidExpression($expr)
     {
-        if(!array_key_exists($expr, $this->allowedExpressions)) {
+        if(!array_key_exists($expr, $this->allowedExpressions))
+        {
             throw new \InvalidArgumentException(sprintf('The expression "%s" is not a valid expression', $expr));
         }
         return true;
     }
 
     /**
-     * Returns an array with the valid expressions to use
+     * Returns an array with the valid expressions to use.
+     *
+     * @returns array The valid expressions.
      */
     public function getValidExpressions()
     {
@@ -76,21 +116,29 @@ class LuceneQueryProcessor
     }
 
     /**
-     * Returns the query to be send to the Lucene Query Engine
+     * Returns the query to be sent to the Lucene Query Engine.
+     *
+     * @returns string The query.
      */
     public function getQuery()
     {
         $query = '';
 
-        foreach($this->allowedExpressions as $expr => $queryExpr) {
+        foreach($this->allowedExpressions as $expr => $queryExpr)
+        {
             $arguments = $this->_args[$expr];
             $inlinedArguments = array();
-            foreach($arguments as $arg) {
+
+            foreach($arguments as $arg)
+            {
                 $inlinedArguments[] = $arg['term'].':'.$arg['value'];
             }
-            if('' !== $query) {
+
+            if('' !== $query)
+            {
                 $query .= $queryExpr;
             }
+
             $query .= implode($queryExpr, $inlinedArguments);
         }
 
