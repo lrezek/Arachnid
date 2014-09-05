2cd1e460d0cc1f0843e6bd670a6961534ec89d17
lrezek <lukas@miratronix.com>
2014-09-04 20:33:42 -0400

    Refactored from lib to src

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Node.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Relation.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Auto.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Start.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/End.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Property.php';
-require __DIR__ . '/lib/LRezek/Neo4PHP/Annotation/Index.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Node.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Relation.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Auto.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Start.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/End.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Property.php';
+require __DIR__ . '/src/LRezek/Neo4PHP/Annotation/Index.php';
